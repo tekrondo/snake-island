5515f482ea7c6eb992751d8ccaada0cc82f407fd
Terwase Kelvin Gberikon <7498753+tekrondo@users.noreply.github.com>
2019-08-01 09:41:18 +0100

    After counting the file and presenting the result, the next task is take the dictionary and find the word with the most appearances and print them out

--- counting.py
+++ counting.py
@@ -5,11 +5,21 @@
 # the key is the word and the value is the number of times it appears in the list.
 wordDict = dict()
 wordsList = list()
+maxDict = dict()
+
 
 newFile = input('Enter the file name: ')
 fhand = open(newFile).read()
-wordList = fhand.split()
+wordList = fhand.rstrip().split()
 
 for word in wordList:
     wordDict[word] = wordList.count(word)
-print(wordDict)
+
+# This section finds the word that appears the most in the dictionary 
+# and prints it. It gets the key using the max method, then assigns 
+# the value by calling the key on the maxDict.
+
+key = max(wordDict)
+maxDict[key] = wordDict.get(key)
+
+print(maxDict)
